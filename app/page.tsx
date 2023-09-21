35bac3a3a05d5e77988bd250a442ccd37ff92691
Marcos Melo <marcosmelo012@gmail.com>
2023-09-21 11:19:49 -0300

    feat: session control

---
+++ app/page.tsx
@@ -0,0 +1,52 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { RedirectType, redirect } from "next/navigation"
+ 
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import { CreateAccountForm } from "@/components/auth/create-account-form"
+
+export default async function Home() {
+
+  let loggedIn = false
+
+  try {
+    
+    const supabase = createServerComponentClient({cookies})
+    
+    const { data: { session },} = await supabase.auth.getSession()
+
+    if(session) loggedIn = true
+
+  } catch (error) {
+
+    console.log("Home", error)
+
+  } finally {
+
+    if(loggedIn) redirect("/user-app", RedirectType.replace)
+
+  }
+
+
+  return (
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <Tabs defaultValue="create-account" className="w-[400px] border rounded-md pb-4 shadow-2xl">
+      <TabsList className="flex justify-around items-center rounded-b-none h-14">
+        <TabsTrigger className="transition-all delay-150" value="create-account">Account</TabsTrigger>
+        <TabsTrigger className="transition-all delay-150" value="login">Login</TabsTrigger>
+      </TabsList>
+      <TabsContent value="create-account">
+        <CreateAccountForm />
+      </TabsContent>
+      <TabsContent value="login">
+      
+      </TabsContent>
+    </Tabs>
+    </div>
+  )
+}
---
+++ app/user-app/page.tsx
@@ -0,0 +1,3 @@
+export default async function UserApp() {
+    return <h1>User Page</h1>
+}
